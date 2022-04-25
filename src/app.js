e09dc662afc66b9863954e915733ab3d9ca27a30
TonaLWC <103759989+TonaLWC@users.noreply.github.com>
2022-04-25 07:11:56 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
-import { LightningElement } from "lwc";
+import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
+    activeSectionMessage = '';
+    isDVisible = false;
+    //Crea variable para mostrar el día hoy en consola
+    fechaLimite = Date.now();
+    hoy = new Date(this.fechaLimite);
+    hoy =  hoy.toDateString();
+   
 
+    handleToggleSection(event) {
+        this.activeSectionMessage =
+            'Revisa  ' + event.detail.openSections;
+    }
 
+    handleToggleDM() {
+        this.isDVisible = !this.isDVisible;
+    }
 
+    get isMessageVisible() {
+        return this.activeSectionMessage.length > 0;
+    }
 }
